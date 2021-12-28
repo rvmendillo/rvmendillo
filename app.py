46ea2f85168535ec19312832d6947c84fc25556c
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2021-12-28 19:50:27 +0800

    Tested Include HTML

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/create', methods=['GET'])
-@app.route('/create/users', methods=['GET'])
-@app.route('/create/users/<username>', methods=['GET'])
 @app.route('/create/users/<username>/<password>', methods=['GET'])
 def create_user(username=None, password=None):
     create_entry(users, {'username': username,
