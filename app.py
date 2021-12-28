c5cf739f5ba0a7a559b91da80c87a136d09caeed
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2021-12-28 19:36:07 +0800

    Added Nested Routes

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/create', methods=['GET'])
+@app.route('/create/users', methods=['GET'])
+@app.route('/create/users/<username>', methods=['GET'])
 @app.route('/create/users/<username>/<password>', methods=['GET'])
-def create_user(username, password):
+def create_user(username=None, password=None):
     create_entry(users, {'username': username,
                          'password': password})
     return f'Created user {username}.'
