d0e9646907175bcd1ec4125ec1c5a3593ab75e66
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-04-02 16:36:46 +0800

    Add skirt sloper

--- app.py
+++ app.py
@@ -37,6 +37,24 @@ def image_to_ascii():
             return 'reCAPTCHA validation failed.'
     return render_template('image_to_ascii.html')
 
+@app.route('/skirt_sloper', methods=['GET', 'POST'])
+def image_to_ascii():
+    if request.method == 'POST':
+        response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={'secret': '6Lfq6-QdAAAAAI6KgavwJfqdPq-FdQFoogEngYTv',
+                                                                                          'response': request.form['g-recaptcha-response']})
+        if response.json()['success']:
+            image_file = request.files['image_file']
+            image_path = 'static/images/' + image_file.filename
+            image_file.save(image_path)
+            image_to_ascii_converter = ImageToASCII(image_path, default_font=True)
+            inverted_colored_ascii_image = image_to_ascii_converter.generate_colored_ascii_image(300)
+            base64_string = image_to_ascii_converter.convert_image_to_base64(inverted_colored_ascii_image)
+            remove(image_path)
+            return render_template('skirt_sloper.html', base64_string=base64_string.decode())
+        else:
+            return 'reCAPTCHA validation failed.'
+    return render_template('skirt_sloper.html')
+
 @app.route('/users/new/<username>/<password>', methods=['GET'])
 def create_user(username=None, password=None):
     create_entry(users, {'username': username,
