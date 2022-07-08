bb99e66eb82a121a22a0846022db3dbc28c9603b
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-07-08 16:16:26 +0800

    Autoscroll

--- static/scripts/realtime_chat.js
+++ static/scripts/realtime_chat.js
@@ -5,8 +5,10 @@ function send_message() {
     var message = document.getElementById("chat_message").value;
     var formatted_message = username + ": " + message;
     socket.send(formatted_message);
+    document.getElementById("chat_message").value = "";
 }
 
 socket.on("message", (formatted_message) => {
     document.getElementById("chat_history").innerHTML += "<p>" + formatted_message + "</p>";
+    document.getElementById("chat_history").scrollTop = document.getElementById("chat_history").scrollHeight;
 })
