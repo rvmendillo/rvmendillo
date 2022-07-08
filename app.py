19d92df71362258e3a84b7be097cbd8b59579737
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-07-08 13:48:56 +0800

    Test sockets

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
@@ -6,4 +7,4 @@ app.jinja_env.trim_blocks = True
 import routes
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=False, host='0.0.0.0', port=5004)
