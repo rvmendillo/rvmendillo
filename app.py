14876f75119c2481a826cb752fa3f32c678ca156
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-15 08:35:10 +0800

    Test CORS

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 import routes
 
