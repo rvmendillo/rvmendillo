800d7eef9e7410475f41d1ac6ec82d0c6559bacc
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-16 12:47:25 +0800

    Updates ASCII output

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -40,7 +40,8 @@ def image_to_ascii():
                                                output_type=output_type,
                                                output=base64_string.decode())
     else:
-        target_height = len(ascii_output.replace('\n', '')) / target_width
+        ascii_output = ascii_output.replace('\n', '\r\n')
+        target_height = len(ascii_output.replace('\r\n', '')) / target_width
         return render_template('project.html', name=project['name'],
                                                category=project['category'],
                                                description=project['description'],
