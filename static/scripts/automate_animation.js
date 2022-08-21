28cf50323790651cf6d0be6facdbe2713b9ff5d5
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-21 11:24:06 +0800

    Update animation

--- static/scripts/automate_animation.js
+++ static/scripts/automate_animation.js
@@ -1,13 +1,13 @@
 function automate_animation() {
-    var h1_list = document.querySelectorAll("section:not(:first-child) .row .column h1");
+    var h1_list = document.querySelectorAll("section:not(:first-of-type) .row .column h1");
     for (var i = 0; i < h1_list.length; i++)
         h1_list[i].setAttribute("data-aos", "fade-up");
 
-    var column_list = document.querySelectorAll("section:not(:first-child) .row:not([class='row center_y']) .column");
+    var column_list = document.querySelectorAll("section:not(:first-of-type) .row:not([class='row center_y']) .column");
     for (var i = 0; i < column_list.length; i++)
         column_list[i].setAttribute("data-aos", "zoom-in");
 
-    var a_list = document.querySelectorAll("section:not(:first-child):not([id='projects']) .row .column .button");
+    var a_list = document.querySelectorAll("section:not(:first-of-type):not([id='projects']) .row .column .button");
     for (var i = 0; i < a_list.length; i++)
         a_list[i].setAttribute("data-aos", "zoom-in");
 
