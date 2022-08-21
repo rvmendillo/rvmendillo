d62cb44d99109973cb8225d10ece6655e2ecc3a7
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-21 11:14:56 +0800

    Update animation

--- static/scripts/automate_animation.js
+++ static/scripts/automate_animation.js
@@ -1,13 +1,13 @@
 function automate_animation() {
-    var h1_list = document.querySelectorAll("section .row .column h1");
+    var h1_list = document.querySelectorAll("section:not(:first-child) .row .column h1");
     for (var i = 0; i < h1_list.length; i++)
         h1_list[i].setAttribute("data-aos", "fade-up");
 
-    var column_list = document.querySelectorAll("section .row:not([class='row center_y']) .column");
+    var column_list = document.querySelectorAll("section:not(:first-child) .row:not([class='row center_y']) .column");
     for (var i = 0; i < column_list.length; i++)
         column_list[i].setAttribute("data-aos", "zoom-in");
 
-    var a_list = document.querySelectorAll("section .row .column a");
+    var a_list = document.querySelectorAll("section:not(:first-child):not([id='projects']) .row .column a");
     for (var i = 0; i < a_list.length; i++)
         a_list[i].setAttribute("data-aos", "zoom-in");
 
