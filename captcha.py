1f5f4cbe4b6c970923adf6f46a0b8e7cfb3fd50e
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-21 13:23:58 +0800

    Hide CAPTCHA key

--- captcha.py
+++ captcha.py
@@ -1,7 +1,8 @@
 from flask import request
 import requests
+import os
 
 def verify_captcha():
-    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={'secret': '6Lfq6-QdAAAAAI6KgavwJfqdPq-FdQFoogEngYTv',
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data={'secret': os.environ['CAPTCHA_SECRET'],
                                                                                       'response': request.form['g-recaptcha-response']})
     return response.json()['success']
