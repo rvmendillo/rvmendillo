845a2933421d4f0df1891400c4e36624d96faa99
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-21 19:45:41 +0800

    Rename library

--- mbti_personality_prediction.py
+++ mbti_personality_prediction.py
@@ -1,13 +1,13 @@
 from app import app
 from flask import render_template, request
 from json import loads
-from mbti_personality_prediction import MBTIPersonalityPrediction
+import mbti_personality_prediction as mbti
 
 @app.route('/mbti_personality_prediction', methods=['GET', 'POST'])
 def mbti_personality_prediction():
     project = loads(request.args['project'])
     paragraph_to_predict = loads(request.args['paragraph_to_predict'])
-    predictor = MBTIPersonalityPrediction()
+    predictor = mbti.MBTIPersonalityPrediction()
     mbti_type = predictor.predict_personality(paragraph_to_predict)
 
     return render_template('project.html', name=project['name'],
