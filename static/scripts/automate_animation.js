d4fd830dc4577b37a98a351941c58edaa931d4ba
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-21 10:41:18 +0800

    Automate animation

---
+++ static/scripts/automate_animation.js
@@ -0,0 +1,13 @@
+function automate_animation() {
+    var h1_list = document.querySelectorAll("section .row .column h1");
+    for (var i = 0; i < h1_list.length; i++)
+        h1_list[i].setAttribute("data-aos", "fade-up");
+
+    var column_list = document.querySelectorAll("section .row:not([class='row center_y']) .column");
+    for (var i = 0; i < column_list.length; i++)
+        column_list[i].setAttribute("data-aos", "zoom-in");
+
+    var a_list = document.querySelectorAll("section .row .column a");
+    for (var i = 0; i < a_list.length; i++)
+        a_list[i].setAttribute("data-aos", "zoom-in");
+}
