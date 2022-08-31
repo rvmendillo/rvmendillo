a420605859bdcb51786ffce7b41d9e3d65c44619
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-08-31 15:13:15 +0800

    Test CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_compress import Compress
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
+CORS(app)
 compress = Compress()
 compress.init_app(app)
 socketio = SocketIO(app)
