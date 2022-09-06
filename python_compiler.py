c17e596f49967ceb277557a433236e8a0d542aa5
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2022-09-06 08:48:39 +0800

    Post method only

--- python_compiler.py
+++ python_compiler.py
@@ -29,7 +29,7 @@ def python_compiler():
                                            output=b64encode(output.decode().encode('ascii')).decode(),
                                            error=b64encode(error.decode().encode('ascii')).decode())
 
-@app.route('/api/python_compiler', methods=['GET', 'POST'])
+@app.route('/api/python_compiler', methods=['POST'])
 def python_compiler_api():
     code_path = save_text_and_get_path(request.json['python_code'], token_hex() + '.py')
     command = f'python {code_path}'
